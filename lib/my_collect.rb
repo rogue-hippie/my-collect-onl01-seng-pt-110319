3ac328200add88e14548dc652ae5ac46234911a7
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-17 01:19:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(empty_array)
   i = 0
   empty_array = []
   while i < array.length
-    empty_array << yield(array[i].upcase)
+    empty_array << yield(array[i])
     i += 1
   end
 end
